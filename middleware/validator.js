7b65679faf354763923a87c9a8e3f463f6fed67d
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-11-01 16:52:36 +0200

    handling errors

--- middleware/validator.js
+++ middleware/validator.js
@@ -2,12 +2,16 @@
 
 function validator(req, res, next) {
     console.log(req.query.name);
+  
    if (req.query.name) {
     next();
    }
     // call next so that tha function in the next line can do its work 
    else{
-       next('Not Found')
+       next(res.status(500.).json({
+           error: 500,
+           type: 'Internal Server Error'
+       }));
    }
   
 }
